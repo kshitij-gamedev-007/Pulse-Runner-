ebc09453496659f0ff48967a2413e7e183e756e4
Kshitij GameDev <kshitijgupta290106@gmail.com>
2025-07-14 19:47:42 +0530

    Added Clamp to player movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,8 @@ public class PlayerControl : MonoBehaviour
     Rigidbody rb;
     Vector3 currentPosition;
     Vector3 newPosition;
+    [SerializeField] float Xclamp = 3f;
+    [SerializeField] float Zclamp = 4f;
     [SerializeField] float moveSpeed = 3f;
     void Awake() {
         rb = GetComponent<Rigidbody>();
@@ -27,6 +29,8 @@ public class PlayerControl : MonoBehaviour
         currentPosition = rb.position;
         Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
         Vector3 movePosition = currentPosition + moveSpeed * Time.fixedDeltaTime * moveDirection;
+        movePosition.x = Mathf.Clamp(movePosition.x, -Xclamp, Xclamp);
+        movePosition.z = Mathf.Clamp(movePosition.z, -Zclamp, Zclamp);
         rb.MovePosition(movePosition);
     }
 }
