5505c88d82c86d5626cb570a5bda518ac12a136c
Kshitij GameDev <kshitijgupta290106@gmail.com>
2025-07-15 16:34:17 +0530

    Insatantiating Random Obstacle

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject[] obstaclePrefabs;
+    [SerializeField] float waitingPeriod = 1f;
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 3.5f;
     void Start()
     {
+        StartCoroutine(WaitSeconds());   
         
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator WaitSeconds()
     {
-        
+        while (true)
+        {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
+            yield return new WaitForSeconds(waitingPeriod);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
+        } 
     }
 }
