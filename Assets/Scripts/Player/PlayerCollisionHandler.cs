ca74397ae19976a494a399b19ee0f971be618997
Kshitij GameDev <kshitijgupta290106@gmail.com>
2025-07-18 17:28:06 +0530

    Adding collision animation

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f; 
+    const string hitString = "Hit";
+    float cooldownTimer = 0f;
+    void Update()
+    {
+        cooldownTimer+= Time.deltaTime;
+    }
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return; 
+
+            animator.SetTrigger(hitString);
+            cooldownTimer = 0f;
     }
 }
